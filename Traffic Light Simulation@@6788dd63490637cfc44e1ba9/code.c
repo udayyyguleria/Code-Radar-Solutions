41021bcd46d2c4623e11651b24e6fdd8d8b53d0c
udayyyguleria <ugx4141@gmail.com>
2025-01-31 18:33:03 +0530

    Solution to Traffic Light Simulation

--- Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -3,16 +3,16 @@ int main() {
     char x ;
     scanf("%c",&x);
 
-    if (x=="R"){
+    if (c=="R"){
         printf("Stop");
     }
 
-    else if (x=="G"){
-        printf("Go")
+    else if (c=="G"){
+        printf("Go");
     }
 
-    else if (x=="Y"){
-        printf("Slow Down")
+    else if (c=="Y"){
+        printf("Slow Down");
     }
 
     else{
