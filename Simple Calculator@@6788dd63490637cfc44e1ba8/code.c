4876623db0d084698bbbd0f401426330fc75e279
udayyyguleria <ugx4141@gmail.com>
2025-01-31 18:51:08 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+int main() {
+    int x,y,r;
+    char z;
+    scanf("%d %d %c",&x ,&y ,&z);
+    if (z=='+'){
+        r=x+y;
+        print("",r)
+    }
+
+    else if(z=='-'){
+        r=x-y;
+        print("",r)
+    }
+
+    else if(z=='*'){
+        r=x*y;
+        print("",r)
+    }
+
+    else if (z=='/'){
+        r=x/y;
+        print("",r)
+    }
+    
+    return 0;
+}
